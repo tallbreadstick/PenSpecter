10bf8074916b99f9240ddbcc53f667679b1f52f2
damascussteel21 <tallbreadstick@gmail.com>
2025-03-10 23:11:39 +0800

    Traceroute Page

---
+++ app/src/main/java/com/tallbreadstick/penspecter/screens/application/Traceroute.kt
@@ -0,0 +1,11 @@
+package com.tallbreadstick.penspecter.screens.application
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Preview
+@Composable
+fun Traceroute(navController: NavController? = null) {
+    
+}
